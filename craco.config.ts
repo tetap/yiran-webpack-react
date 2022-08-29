bd810c37fe5957ef808b59e0e3d20f794512fc1f
zyc <239559238@qq.com>
2022-08-29 17:43:23 +0800

    拆

--- craco.config.ts
+++ craco.config.ts
@@ -1,9 +1,14 @@
 const path = require('path')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
   webpack: {
+    plugins: [new NodePolyfillPlugin()],
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  devServer: {
+    open: false
   }
 }
