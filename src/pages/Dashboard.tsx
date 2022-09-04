923c2507965b5f21d12bbc1ffe7e9bc92c365122
yiran <239559238@qq.com>
2022-09-04 13:54:52 +0800

    ✨ feat: cssinjs

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { css } from '@emotion/react'
 
+export const Container = css({
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: '#ffffff',
+  overflow: 'hidden',
+  position: 'relative',
+  canvas: {
+    '-webkit-font-smoothing': 'none'
+  }
+})
 export default function Dashboard() {
   const { t } = useTranslation()
   return (
     <>
-      <div>{t('title')}</div>
+      <div css={Container}>{t('title')}</div>
     </>
   )
 }
